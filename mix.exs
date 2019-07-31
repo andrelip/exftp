9028a2db8e54486d4a77d3fbc70f7fb4d8ef0566
André Souza <andrestephano@gmail.com>
2019-07-31 09:30:42 -0300

    add ftp as application

--- mix.exs
+++ mix.exs
@@ -2,23 +2,25 @@ defmodule Ftp.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :exftp,
-     version: "0.1.0",
-     build_path: "_build",
-     config_path: "config/config.exs",
-     deps_path: "deps",
-     lockfile: "mix.lock",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :exftp,
+      version: "0.1.0",
+      build_path: "_build",
+      config_path: "config/config.exs",
+      deps_path: "deps",
+      lockfile: "mix.lock",
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :ftp]]
   end
 
   # Dependencies can be Hex packages:
